7acca38d607edfe5287952808a2e574a69f7a6cb
TommyWNE <web.tommasovene@gmail.com>
2025-01-09 14:40:31 +0700

    updated template files

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,18 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
 import htmlPurge from "vite-plugin-html-purgecss";
 
-export default {
+export default defineConfig({
+    root: resolve(__dirname, "src/"),
+    build: {
+        outDir: resolve(__dirname, "dist"),
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "src/index.html"),
+                about: resolve(__dirname, "src/pages/about/index.html"),
+            },
+        },
+    },
     plugins: [htmlPurge()],
-};
+});
